bd01ef2a9256babcb62d6e98b7ca17298b03a776
xinhai <xinhai@zhangyue.com>
2020-09-20 21:47:02 +0800

    有效异位分组

--- Week_02/49.py
+++ Week_02/49.py
@@ -1,3 +1,4 @@
+# pylint: disable=all
 """
 49. 字母异位词分组
 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
@@ -16,3 +17,25 @@
 所有输入均为小写字母。
 不考虑答案输出的顺序。
 """
+# 1. 暴力 排序
+# 2. hash 验证异位
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for string in strs:
+            ans[tuple(sorted(string))].append(string)
+        return ans.values()
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for string in strs:
+            an = [0] * 26
+            for i in string:
+                an[ord(i) - ord("a")] += 1
+            ans[an].append(i)
+        return list(ans.values())
